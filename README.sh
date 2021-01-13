32e73af58d94bdc56caf03099d6ba5e1e79a2a6a
Kyle Gordon <kyle@lodge.glasgownet.com>
2021-01-13 20:32:56 +0000

    Correct cronjobs source

--- README.sh
+++ README.sh
@@ -11,26 +11,30 @@
 TFMVER=2.4.3
 
 sudo apt-get update
-sudo apt-get install -y nfs-common openssh-server wget nginx
+sudo apt-get install -y git
+git clone https://github.com/kylegordon/pictureframe ~/PicturePi/
+sudo apt-get install -y nfs-common openssh-server wget nginx php-fpm
 sudo /etc/init.d/ssh restart
 sudo apt-get install -y feh lightdm raspberrypi-ui-mods watchdog vim
+sudo apt-get remove --purge xscreensaver xscreensaver-data
+
+sudo systemctl restart php7.3-fpm.service
 
 wget https://github.com/prasathmani/tinyfilemanager/archive/${TFMVER}.tar.gz -O /tmp/tinyfilemanager.tar.gz
 tar -zxvf /tmp/tinyfilemanager.tar.gz --directory /tmp/
-mv /tmp/tinyfilemanager-${TFMVER}/* /var/www/
-
-#cp pictureframe.desktop /etc/xdg/autostart/
+sudo mv /tmp/tinyfilemanager-${TFMVER}/* /var/www/html/
 
 sudo cp ~/PicturePi/autostart /etc/xdg/lxsession/LXDE/autostart
 sudo cp ~/PicturePi/autostart /etc/xdg/lxsession/LXDE-pi/autostart
 sudo cp ~/PicturePi/pictureframe.desktop /etc/xdg/autostart/
+sudo cp ~/PicturePi/nginx-site.conf /etc/nginx/sites-available/default
 
 echo '172.24.32.5:/srv/nfs4/store/home/Pictures/PictureFrames/ /home/pi/Pictures/ nfs defaults,_netdev,vers=4,async 0 0' | sudo tee -a /etc/fstab
 
 # Use sudo raspi-config again to enable auto-login to desktop
 
-sudo cp cronjobs/morning_reboot /etc/cron.d/morning_reboot
-sudo cp cronjobs/on_off_schedule /etc/cron.d/on_off_schedule
+sudo cp ~/PicturePi/cronjobs/morning_reboot /etc/cron.d/morning_reboot
+sudo cp ~/PicturePi/cronjobs/on_off_schedule /etc/cron.d/on_off_schedule
 
 # Use something to maintain a read-only SD card
 http://blog.pi3g.com/2014/04/make-raspbian-system-read-only/
