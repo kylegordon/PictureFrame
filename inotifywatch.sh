b1d7bdb836eb5f6a3dcd1c0775a54435177ab17d
Kyle Gordon <kyle@lodge.glasgownet.com>
2021-02-14 10:25:24 +0000

    Copying a file, not a directory

--- inotifywatch.sh
+++ inotifywatch.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 ## The target and source can contain spaces as long as they are quoted.
-target="/etc/cron.d/schedule"
 source="/var/www/html/Pictures/schedule.txt";
+target="/etc/cron.d/schedule"
 
 while true; do
   ## Watch for new files, the grep will return true if a file has
@@ -12,5 +12,5 @@ while true; do
   ## The -u option to cp causes it to only copy files
   ## that are newer in $source than in $target. Any files
   ## not present in $target will be copied.
-  cp -vu "$source"/* "$target"/
+  cp -vu "$source" "$target"
 done
