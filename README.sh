128909803ec058cfdf294a77abc6a505c76bb242
Kyle Gordon <kyle@lodge.glasgownet.com>
2021-02-27 17:01:45 +0000

    Fix ownership

--- README.sh
+++ README.sh
@@ -35,6 +35,7 @@ git clone https://github.com/kylegordon/pictureframe ~/PicturePi/
 wget http://sye.dk/sfpg/Single_File_PHP_Gallery_${SFPGVER}.zip -O /tmp/sfpg.zip
 sudo unzip /tmp/sfpg.zip index.php -d /var/www/html/
 sudo chown www-data. /var/www/html/index.php
+sudo chown www-data. /var/www/html/
 
 # Patch the configuration file to our own taste
 patch -d /var/www/html/ < index.patch
