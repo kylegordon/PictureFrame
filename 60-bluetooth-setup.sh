6276fde4b4ff88dbe59a8660110af5a839afe36e
Kyle Gordon <kyle@lodge.glasgownet.com>
2021-05-22 10:59:57 +0100

    Add obexpushd unit file

--- 60-bluetooth-setup.sh
+++ 60-bluetooth-setup.sh
@@ -2,8 +2,10 @@ sudo apt-get -y install obexpushd
 
 sudo systemctl stop bluetooth
 sudo cp bluetooth-configs/bluetooth-compat.conf /etc/systemd/system/bluetooth.service.d/compat.conf
+sudo cp bluetooth-configs/obexpushd.service /etc/systemd/system/obexpushd.service
 sudo systemctl daemon-reload
 sudo systemctl start bluetooth
+sudo systemctl enable obexpushd; sudo systemctl start obexpushd
 
 sudo bluetoothctl <<EOF
 power on
