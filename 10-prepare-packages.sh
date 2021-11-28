6757cbd23847df632a4bc807210df4178dcbdf93
Kyle Gordon <kyle@glasgownet.com>
2021-11-28 21:30:45 +0000

    Additional packages

--- 10-prepare-packages.sh
+++ 10-prepare-packages.sh
@@ -12,7 +12,11 @@ sudo apt-get install -y git \
                         lightdm \
                         raspberrypi-ui-mods \
                         watchdog \
-                        vim
+                        vim \
+                        build-essential \
+                        cmake
 
 # Remove xscreensaver so that it doesn't take over the screen
-sudo apt-get remove --purge xscreensaver xscreensaver-data pulseaudio
+sudo apt-get remove --purge xscreensaver xscreensaver-data \
+                            pulseaudio libpulse0 pulseaudio-utils \
+                            cups cups-browsed cups-client cups-core-drivers cups-daemon cups-filters cups-filters-core-drivers cups-ipp-utils cups-server-common cups-common cups-pk-helper cups-ppdc python3-cups
