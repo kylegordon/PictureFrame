c9e76713c72715b9caffd855c0692d3eab69423b
Kyle Gordon <kyle@glasgownet.com>
2021-12-12 15:47:15 +0000

    Include autoconf

--- 65-opendrop-setup.sh
+++ 65-opendrop-setup.sh
@@ -1,4 +1,4 @@
-sudo apt-get install raspberrypi-kernel-headers git libgmp3-dev gawk qpdf bison flex make
+sudo apt-get install raspberrypi-kernel-headers git libgmp3-dev gawk qpdf bison flex make autoconf
 sudo apt-get remove --purge wpasupplicant # Maybe use this instead? - sudo systemctl mask wpa_supplicant ; sudo systemctl stop wpa_supplicant.service
 
 ./build-nexmon.sh
