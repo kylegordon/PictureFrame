795b24e94538c6bab1ac0ddda94b61ef5c5a2703
Kyle Gordon <kyle@glasgownet.com>
2021-12-12 16:10:18 +0000

    Remove fluff

--- 20-configure-lighttpd.sh
+++
@@ -1,7 +0,0 @@
-cp lighttpd-configs/15-fastcgi-php.conf /etc/lighttpd/conf-available/15-fastcgi-php.conf
-
-lighttpd-enable-mod fastcgi
-lighttpd-enable-mod fastcgi-php
-
-service lighttpd force-reload
-systemctl restart php7.3-fpm.service
