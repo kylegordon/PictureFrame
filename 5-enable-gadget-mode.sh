e282ee964aa3955142154e98df60c97e5cf454ba
Kyle Gordon <kyle@glasgownet.com>
2021-12-27 21:21:02 +0000

    More gadget mode fixes

--- 5-enable-gadget-mode.sh
+++ 5-enable-gadget-mode.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 
-if lsb_release -c | grep -q stretch; then
-    echo "Patching for stretch"
-    patch -d /boot/ < config.stretch.patch
-elif lsb_release -c | grep -q bullseye; then
-    echo "Patching for bullseye"
-    patch -d /boot/ < config.bullseye.patch
+if grep -q "dtcoverlay=dwc2" /boot/config.txt; then
+    echo "Gadget mode already enabled"
 else
-    echo "Unknown distro, not patching"
+    echo "Enabling gadget mode : patching config.txt"
+    if lsb_release -c | grep -q stretch; then
+        echo "Patching for stretch"
+        sudo patch -d /boot/ < config.stretch.patch
+    elif lsb_release -c | grep -q bullseye; then
+        echo "Patching for bullseye"
+        sudo patch -d /boot/ < config.bullseye.patch
+    else
+        echo "Unknown distro, not patching"
+    fi
 fi
 
-# Use sed to append, not overwrite, the following to the end of /boot/config.txt
-# PARTUUID changes with every install, so patch/diff will never work
-sudo sed '$s/$/ modules-load=dwc2,g_ether/' /boot/cmdline.txt
+if grep -q "modules-load=dwc2,g_ether" /boot/cmdline.txt; then
+    echo "Gadget mode already enabled"
+else
+    echo "Enabling gadget mode : patching cmdline.txt"
+    # Use sed to append, not overwrite, the following to the end of /boot/config.txt
+    # PARTUUID changes with every install, so patch/diff will never work
+    sudo sed -i '$s/$/ modules-load=dwc2,g_ether/' /boot/cmdline.txt
+fi
+
