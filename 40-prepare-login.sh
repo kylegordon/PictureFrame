f7010e92586e55a707e53f5edcbb92dc9d2e4e1d
Kyle Gordon <kyle@glasgownet.com>
2021-12-27 22:06:45 +0000

    Relocate to end

--- 40-prepare-login.sh
+++ 40-prepare-login.sh
@@ -1,6 +1,3 @@
-# Enable autologin for user. Can only be done once lightdm is installed.
-sudo raspi-config nonint do_boot_behaviour B4 # Enable GUI and autologin
-
 # Place relevant symlinks to enable autostart of LXDE
 sudo cp ~/PicturePi/autostart /etc/xdg/lxsession/LXDE/autostart
 sudo cp ~/PicturePi/autostart /etc/xdg/lxsession/LXDE-pi/autostart
@@ -9,3 +6,6 @@ sudo cp ~/PicturePi/pictureframe.desktop /etc/xdg/autostart/
 # Should do the same job as setting raspi-config autologin to Graphical Pi Desktop
 sudo rm /etc/systemd/system/default.target
 sudo ln -s /lib/systemd/system/graphical.target /etc/systemd/system/default.target
+
+# Enable autologin for user. Can only be done once lightdm is installed.
+sudo raspi-config nonint do_boot_behaviour B4 # Enable GUI and autologin
