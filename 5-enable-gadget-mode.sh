13455d4e027ba9a904bcdde3934f0ab1a512c868
Kyle Gordon <kyle@glasgownet.com>
2021-12-27 21:10:07 +0000

    Tidy patching

--- 5-enable-gadget-mode.sh
+++ 5-enable-gadget-mode.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-if [ -f /sys/kernel/debug/usb/gadget/g1/UDC ]; then
-    echo "gadget" > /sys/kernel/debug/usb/gadget/g1/UDC
-elif [ -f /sys/kernel/debug/usb/gadget/g1/functions/rndis.usb0/os_desc/b_vendor_code ]; then
-    echo "rndis" > /sys/kernel/debug/usb/gadget/g1/functions/rndis.usb0/os_desc/b_vendor_code
+if lsb_release -c | grep -q stretch; then
+    echo "Patching for stretch"
+    patch -d /boot/ < config.stretch.patch
+elif lsb_release -c | grep -q bullseye; then
+    echo "Patching for bullseye"
+    patch -d /boot/ < config.bullseye.patch
+else
+    echo "Unknown distro, not patching"
 fi
-
-patch -d /boot/ < config.stretch.patch
 
 # Use sed to append, not overwrite, the following to the end of /boot/config.txt
 # PARTUUID changes with every install, so patch/diff will never work
